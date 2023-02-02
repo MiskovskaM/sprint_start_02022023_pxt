ab9a588ed71b69e95b8c886b00fb1ee60b02d75f
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2023-02-02 10:26:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+//startovací brána: startovací odpočet ( 3, 2, 1 + zvuk)
+                  //zaznamenat po odpočtu Start
+//  cílová brána : zaznamenat doběh do cíle + informace (čas)
+//startovací + cílová :zobrazit výsledky na vyžádání
+
+//startovací: možnost opětovaného spuštění + resert cílové brány
+
+//radio.band každá dvojice jiný + group
+
+
+
